0b845567efb338381d03c75efd1668ef75a61bd2
Roan Kattouw <roan.kattouw@gmail.com>
2021-03-03 15:15:03 -0500

    Add Jest tests and snapshots
    - Exclude snapshot files from eslint and TypeScript checks
    - Copy Jest config and helpers from WVUI
    - Copy Button.test.ts from WVUI

---
+++ src/components/button/Button.snap.ts
@@ -0,0 +1,61 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`matches the snapshot Case 0 No props and no slot: ({}) => HTML 1`] = `
+<button
+  class="wvui-button wvui-button--default wvui-button--framed"
+>
+  
+  
+  
+</button>
+`;
+
+exports[`matches the snapshot Case 1 default action: ({"action": "default"}) => HTML 1`] = `
+<button
+  class="wvui-button wvui-button--default wvui-button--framed"
+>
+  
+  
+  
+</button>
+`;
+
+exports[`matches the snapshot Case 2 progressive action: ({"action": "progressive"}) => HTML 1`] = `
+<button
+  class="wvui-button wvui-button--progressive wvui-button--framed"
+>
+  
+  
+  
+</button>
+`;
+
+exports[`matches the snapshot Case 3 destructive action: ({"action": "destructive"}) => HTML 1`] = `
+<button
+  class="wvui-button wvui-button--destructive wvui-button--framed"
+>
+  
+  
+  
+</button>
+`;
+
+exports[`matches the snapshot Case 4 Quiet: ({"quiet": true}) => HTML 1`] = `
+<button
+  class="wvui-button wvui-button--default wvui-button--quiet"
+>
+  
+  
+  
+</button>
+`;
+
+exports[`matches the snapshot Case 5 Slotted: ({}) => HTML 1`] = `
+<button
+  class="wvui-button wvui-button--default wvui-button--framed"
+>
+  
+  <stub />
+  
+</button>
+`;
