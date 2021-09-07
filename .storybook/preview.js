28941eda3434c17e7f2240b753311447d601732d
Anne Tomasevich <atomasevich@wikimedia.org>
2021-09-07 15:31:12 -0400

    Add and configure toolbar addon
    - Adds configuration for a locale toolbar item, which allows the
    user to select a language
    - Adds configuration for a direction toolbar item, which allows the
    user to select a text directionality
    - Uses a decorator to set the `lang` and `dir` attrbutes on the
    `<html>` element based on these values

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,3 +9,49 @@ export const parameters = {
     },
   },
 }
+
+// Set up language and direction swtichers in the toolbar.
+export const globalTypes = {
+  locale: {
+    name: 'Locale',
+    description: 'Internationalization locale',
+    defaultValue: 'en',
+    toolbar: {
+      icon: 'globe',
+      items: [
+        { value: 'en', title: 'English' },
+        { value: 'es', title: 'Español' },
+		{ value: 'he', title: 'עברית' }
+      ],
+    },
+  },
+
+  direction: {
+	  name: 'Direction',
+	  description: 'Text directionality',
+	  defaultValue: 'ltr',
+	  toolbar: {
+		  icon: 'book',
+		  items: [
+			  { value: 'ltr', right: '(ltr)', title: 'Left to right' },
+			  { value: 'rtl', right: '(rtl)', title: 'Right to left' }
+		  ]
+	  }
+  }
+};
+
+// A decorator is usually used to wrap stories in additional elements. However,
+// we can use it here to set the `lang` and `dir` attributes on the <html> tag
+// when a story mounts, which happens every time one of these attributes is
+// changed by the user via the toolbar.
+export const decorators = [ ( story, context ) => ( {
+  components: { story },
+  data() {
+	  return { context }
+  },
+  mounted() {
+	window.document.documentElement.lang = this.context.globals.locale;
+	window.document.documentElement.dir = this.context.globals.direction;
+  },
+  template: '<story />'
+} ) ];
